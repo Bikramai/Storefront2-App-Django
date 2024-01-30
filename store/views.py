944ec1154c4eaeef87572e96e0dc1b54ab0cacaf
bikramia <bikram.rhce@gmail.com>
2024-01-29 22:30:51 -0500

    Creating API Views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+# Create your views here. It's function that takes the resquest and return the response
+@api_view()
+def product_list(request):
+    return Response('ok')
+
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
