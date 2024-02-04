bc9fb52f28f89b79be843db2abcf9ee6fe902703
bikramia <bikram.rhce@gmail.com>
2024-02-04 11:14:39 -0500

    Serialize Relationships-Option4 way-Hyperlink

--- store/serializers.py
+++ store/serializers.py
@@ -12,7 +12,10 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name='collection-detail'
+    )
         
     
 
