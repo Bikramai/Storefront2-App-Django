f4727016641aeba53158ff1e72ef71d3203a08cf
bikramia <bikram.rhce@gmail.com>
2024-02-04 11:00:32 -0500

    Serialize Relationships-Option2 way

--- store/serializers.py
+++ store/serializers.py
@@ -12,9 +12,8 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset=Collection.objects.all()
-    )
+    collection = serializers.StringRelatedField()
+        
     
 
 
