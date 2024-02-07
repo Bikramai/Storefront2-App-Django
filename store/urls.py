ea1af793d847125fadca64fe69faa29ec3915805
bikramia <bikram.rhce@gmail.com>
2024-02-07 15:16:01 -0500

    Filtering

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 # pprint(router.urls) if you want to print in terminal 
 
