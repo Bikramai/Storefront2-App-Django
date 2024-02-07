b5e9dc77859d63e2142dcbf3466874c420c1e888
bikramia <bikram.rhce@gmail.com>
2024-02-07 13:34:50 -0500

    Routers

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
+# from pprint import pprint
+
+
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+# pprint(router.urls) if you want to print in terminal 
+
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'), 
-]
+urlpatterns = router.urls
+# [
+    # path('products/', views.ProductList.as_view()),
+    # path('products/<int:pk>/', views.ProductDetail.as_view()),
+    # path('collections/', views.CollectionList.as_view()),
+    # path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'), 
+# ]
--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from django.db.models.aggregates import Count
-from .models import Product, Collection
+from .models import Product, OrderItem, Collection
 from .serializers import CollectionSerializer, ProductSerializer
 
 
@@ -20,13 +20,12 @@ class ProductViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'request': self.request}
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=id)
-        if Product.orderitems.count() > 0:
+    
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted becauseit is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        return super().destroy(request, *args, **kwargs)
 
 
 class CollectionViewSet(ModelViewSet):
