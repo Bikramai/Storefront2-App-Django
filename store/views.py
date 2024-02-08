b4023eb37f37cabf80c0c06b08c76b6821567e6e
bikramia <bikram.rhce@gmail.com>
2024-02-07 21:03:53 -0500

    Fix Delete

--- store/views.py
+++ store/views.py
@@ -28,11 +28,12 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def destroy(self, request, *args, **kwargs):
-        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
-            return Response({'error': 'Product cannot be deleted becauseit is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-        return super().destroy(request, *args, **kwargs)
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -41,9 +42,9 @@ class CollectionViewSet(ModelViewSet):
     serializer_class = CollectionSerializer
 
     def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=id)
+        collection = get_object_or_404(Collection, pk=pk)
         if Collection.orderitems.count() > 0:
-            return Response({'error': 'Product cannot be deleted becauseit is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
